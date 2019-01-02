2c906baa0f355885339b42205623cfd5f83e297f
JCamayo <jcamayo@idonia.com>
2019-01-02 16:44:20 +0100

    Added song list endponints and create song endpoints on backend

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,16 +1,29 @@
 <template>
   <v-layout>
       <v-flex xs6 offset-xs3>
-        <panel title="Songs"></panel>
+        <panel title="Songs">
+          <div v-for="(song, index) in songs" :key="index">
+            {{song.title}} <br>
+            {{song.artist}} <br>
+            {{song.album}} <br>
+          </div>
+        </panel>
       </v-flex>
     </v-layout>
 </template>
 <script>
 import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
 export default {
   name: 'Login',
   components: {
     Panel
+  },
+  data: () => ({
+    songs: null
+  }),
+  async mounted () {
+    this.songs = (await SongsService.index()).data
   }
 }
 </script>

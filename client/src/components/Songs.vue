a761d9c83cfb5deaf48a47416faec752eb8f7ca9
JCamayo <jcamayo@idonia.com>
2019-06-10 18:10:53 +0200

    added cards to list songs

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -12,35 +12,33 @@
           >
             <v-icon>add</v-icon>
           </v-btn>
-          <v-list two-line>
-            <template>
-              <v-subheader>
-                List of songs
-              </v-subheader>
-              <div v-for="(song, index) in songs" :key="index">
-                <v-divider></v-divider>
-                <v-list-tile
-                  avatar
-                >
-                  <v-list-tile-avatar>
-                    <img :src="song.albumImgUrl">
-                  </v-list-tile-avatar>
-                  <v-list-tile-content>
-                    <v-list-tile-title><strong>{{ song.title }}</strong> | {{ song.genre }}</v-list-tile-title>
-                    <v-list-tile-sub-title><span class='text--primary'>{{ song.artist }}</span> - <small>{{ song.album }}</small></v-list-tile-sub-title>
-                  </v-list-tile-content>
-                  <v-btn
-                    dark
-                    class="cyan"
-                    @click="navigateTo({ name: 'song', params: { songId: song.id } })"
-                  >
-                    View Song
-                  </v-btn>
-                </v-list-tile>
-
-              </div>
-            </template>
-          </v-list>
+          <v-subheader>
+            List of songs
+          </v-subheader>
+          <v-card color="cyan" dark class="mt-2" v-for="(song, index) in songs" :key="index">
+            <v-layout row>
+              <v-flex xs7>
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline">{{ song.title }}</div>
+                    <div><span>{{ song.artist }}</span> - <small>{{ song.album }}</small></div>
+                    <div>{{ song.genre }}</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+              <v-flex xs5>
+                <v-img
+                  src="https://placehold.it/100x100"
+                  height="100px"
+                  contain
+                ></v-img>
+              </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+            <v-card-actions>
+              <v-btn flat dark @click="navigateTo({ name: 'song', params: { songId: song.id } })">View Song</v-btn>
+            </v-card-actions>
+          </v-card>
         </panel>
       </v-flex>
     </v-layout>
--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,20 +1,52 @@
 <template>
-  <div>
-    I AM A SONG
-  </div>
+  <v-layout v-if="song">
+    <v-flex xs4>
+      <panel title="Song Metadata">
+        <v-list two-line subheader>
+          <v-subheader>General</v-subheader>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Title</v-list-tile-title>
+              <v-list-tile-sub-title>{{ song.title }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Artist</v-list-tile-title>
+              <v-list-tile-sub-title>{{ song.artist }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Genre</v-list-tile-title>
+              <v-list-tile-sub-title>{{ song.genre }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </panel>
+    </v-flex>
+    <v-flex xs8>
+      <panel title="Song Details" class="ml-4">
+        {{ song.title }}
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 import SongsService from '@/services/SongService'
 export default {
   name: 'ViewSong',
+  components: {
+    Panel
+  },
   data: () => ({
     song: null
   }),
   async mounted () {
     const songId = this.$store.state.route.params.songId
-    this.song = await SongsService.show(songId)
-    console.log('si')
+    this.song = (await SongsService.show(songId)).data
   }
 }
 </script>
